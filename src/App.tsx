9d7f730fb8a943cc80fc78ec9addf8852150f17c
Maksim-Attsetski <maks2001maks2001@gmail.com>
2022-06-20 14:47:29 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Col, Row } from 'antd';
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <Row align={'middle'} justify={'center'} className={'page'}>
+          <Col>
+              <p>calc</p>
+          </Col>
+        </Row>
+    );
+};
+
+export default App;
